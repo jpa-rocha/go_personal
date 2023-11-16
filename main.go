133b80aa693b1bd51a88235c017cec9cca92a31d
Joao Rocha <janos.rocha@gmail.com>
2023-11-17 00:40:54 +0100

    feat(cobra): started implementing cobra

--- main.go
+++ main.go
@@ -1,20 +1,9 @@
-package server 
+package main
 
 import (
-	// "html/template"
-	"net/http"
-	"os/exec"
-	"fmt"
+	"adamastor/cmd"
 )
 
-
-const dirname = "./"
-
 func main() {
-	http.Handle("/", http.FileServer(http.Dir(dirname)))
-
-	exec.Command("xdg-open", "http://localhost:8000").Run()
-	fmt.Println("Server started at http://localhost:8000")
-	http.ListenAndServe(":8000", nil)
-
+    cmd.RootCmd.Execute()
 }
