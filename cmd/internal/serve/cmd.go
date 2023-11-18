b22dfaaec23f20c0e8352453fdc93a9f5354fced
Joao Rocha <janos.rocha@gmail.com>
2023-11-18 13:22:15 +0100

    refactor(server): implemented changes to work with the server struct

--- cmd/internal/serve/cmd.go
+++ cmd/internal/serve/cmd.go
@@ -2,7 +2,6 @@ package serve
 
 import (
 	"embed"
-	"io/fs"
 	"log"
 	"net/http"
 	"time"
@@ -14,43 +13,31 @@ import (
 const timeOut = 10
 const port = ":8000"
 
+// TODO: is embeded needed is there a non global solution?
 //go:embed public
 var content embed.FS
 
 func RunServe(_ *cobra.Command, _ []string) error {
-	server := &http.Server{
-		Addr:         port,
-		ReadTimeout:  timeOut * time.Second,
-		WriteTimeout: timeOut * time.Second,
-	}
+	server := NewServer(
+        content, 
+        &http.Server{
+		    Addr:         port,
+		    ReadTimeout:  timeOut * time.Second,
+	    	WriteTimeout: timeOut * time.Second,
+	    },
+    )
 
-	setStaticPaths()
-    http.HandleFunc("/", handleRoutes)
+    server.setStaticPaths()
+    if server.Err != nil {
+        log.Printf("error: a problem occurred setting the file system: %s", server.Err.Error())
+        return server.Err 
+    }
+    http.HandleFunc("/", server.ServeHTTP)
     log.Println("Server started at http://localhost" + port)
     
-	log.Fatal(server.ListenAndServe())
+	log.Fatal(server.Config.ListenAndServe())
 
 	return nil
-}
-
-// setStaticPath determines which folders inside the public folder will be included.
-func setStaticPaths() {
-    paths := []string{
-        "/static/",
-        "/templates",
-    }
-    handlePaths(paths) 
-}
-
-func handlePaths(paths []string) {
-	staticFS, err := fs.Sub(content, "public")
-	if err != nil {
-		log.Fatal(err)
-	}
-    
-    for _, path := range paths {
-        http.Handle(path, http.StripPrefix(path, http.FileServer(http.FS(staticFS))))
-    }
 }
 
 
--- cmd/internal/serve/handleRoutes.go
+++ cmd/internal/serve/handleRoutes.go
@@ -1,20 +1,18 @@
 package serve
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "html/template"
+	"embed"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
 )
 
-func handleRoutes(w http.ResponseWriter, r *http.Request) {    
-    if r.URL.Path == "/" {
-        handleIndex(w, r)
-    }
-}
 
-func handleIndex(w http.ResponseWriter, r *http.Request) {    
-    tmpl, err := getTmpl("public/index.html")
+func handleIndex(w http.ResponseWriter, _ *http.Request, content embed.FS) {    
+    fmt.Println(os.Getwd())
+    tmpl, err := getTmpl("index.html", content)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,10 +20,9 @@ func handleIndex(w http.ResponseWriter, r *http.Request) {
     if err := tmpl.Execute(w, nil); err != nil {
 			log.Fatal(err)
 	}
-
 }
 
-func getTmpl(file string) (*template.Template, error) {
+func getTmpl(file string, content embed.FS) (*template.Template, error) {
 	tmpl := template.New("index")
 
 	fileContents, err := content.ReadFile(file)
