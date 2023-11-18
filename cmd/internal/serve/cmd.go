e255aa43f1461c60a54f50942eb4f3618ff6e702
Joao Rocha <janos.rocha@gmail.com>
2023-11-18 02:03:25 +0100

    feat(server): used embed

--- cmd/internal/serve/cmd.go
+++ cmd/internal/serve/cmd.go
@@ -1,32 +1,56 @@
 package serve
 
 import (
-    "net/http"
-    "time"
-    "log"
-    "github.com/spf13/cobra"
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/spf13/cobra"
 )
 
+// TODO: need to come from config
 const timeOut = 10
 const port = ":8000"
-const dirname = "./"
+
+//go:embed public
+var content embed.FS
 
 func RunServe(_ *cobra.Command, _ []string) error {
-
-	// setServePath()
-    http.Handle("/", http.FileServer(http.Dir(dirname)))
-
-	log.Println("Server started at http://localhost" + port)
-
-	s := &http.Server{
+	server := &http.Server{
 		Addr:         port,
 		ReadTimeout:  timeOut * time.Second,
 		WriteTimeout: timeOut * time.Second,
 	}
 
-	log.Fatal(s.ListenAndServe())
+	setStaticPaths()
+    http.HandleFunc("/", handleRoutes)
+    log.Println("Server started at http://localhost" + port)
+    
+	log.Fatal(server.ListenAndServe())
 
 	return nil
+}
+
+// setStaticPath determines which folders inside the public folder will be included.
+func setStaticPaths() {
+    paths := []string{
+        "/static/",
+        "/templates",
+    }
+    handlePaths(paths) 
+}
+
+func handlePaths(paths []string) {
+	staticFS, err := fs.Sub(content, "public")
+	if err != nil {
+		log.Fatal(err)
+	}
+    
+    for _, path := range paths {
+        http.Handle(path, http.StripPrefix(path, http.FileServer(http.FS(staticFS))))
+    }
 }
 
 
